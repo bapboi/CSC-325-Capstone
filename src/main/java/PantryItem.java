6f6579a292f6f98c5debfc618e622306c63e472e
Zeshan Asif <asifz22@farmingdale.edu>
2026-06-21 15:51:04 -0400

    Add PantryItem model class

---
+++ src/main/java/PantryItem.java
@@ -0,0 +1,11 @@
+public class PantryItem {
+
+    private Ingredient ingredient;
+
+    public PantryItem() {
+    }
+
+    public PantryItem(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
